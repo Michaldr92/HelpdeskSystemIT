09a4a30431c1e8238a8452a04fc36856ee0e7ff3
Michał <m.drop@interia.pl>
2017-08-16 14:12:17 +0200

    Update AdminGUI.java

--- src/pl/mdrop/app/AdminGUI.java
+++ src/pl/mdrop/app/AdminGUI.java
@@ -8,21 +8,6 @@ public class AdminGUI {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AdminGUI window = new AdminGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
